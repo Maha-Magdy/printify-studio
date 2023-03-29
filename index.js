0ac4589a48922242177c69727f79f4815d4d0117
maha-magdy <maha.magdy@reviewsolicitors.com>
2023-03-29 10:30:07 +0100

    Handle the onclick function on the btn to open the file dialogue

---
+++ index.js
@@ -0,0 +1,6 @@
+const choosePhotoBtn = document.querySelector('.choose-photo');
+const choosePhotoFileInput = document.querySelector('.choose-photo-file-input');
+
+const choosePhotoBtnHandler = () => {
+    choosePhotoFileInput.click();
+};
