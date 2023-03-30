08cfdd4989e50067f4681413b28be20d58ec01bf
maha-magdy <maha.magdy@reviewsolicitors.com>
2023-03-30 10:50:29 +0100

    Work on the functionality of adding the photo

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const choosePhotoBtn = document.querySelector('.choose-photo');
 const choosePhotoFileInput = document.querySelector('.choose-photo-file-input');
+const chosenPhoto = document.querySelector('.chosenPhoto');
 
 const choosePhotoBtnHandler = () => {
     choosePhotoFileInput.click();
 };
+
+const fileInputOnchangeHandler = () => {
+    const image = URL.createObjectURL(choosePhotoFileInput.files[0]);
+    chosenPhoto.src = image;
+    chosenPhoto.onload = () => {
+        URL.revokeObjectURL(image);
+    }
+}
