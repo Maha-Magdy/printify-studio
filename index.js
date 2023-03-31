dc6b4a8e70d3ae5b7f5829c51b9790666da5c825
Maha Magdy <mahamagdy3@gmail.com>
2023-03-31 08:14:02 +0200

    Add clear button, and work on the structure of the image

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const choosePhotoBtn = document.querySelector('.choose-photo');
-const choosePhotoFileInput = document.querySelector('.choose-photo-file-input');
-const chosenPhoto = document.querySelector('.chosenPhoto');
+const choosePhotoBtn = document.querySelector(".choose-photo-btn");
+const choosePhotoFileInput = document.querySelector(".choose-photo-file-input");
+const chosenPhoto = document.querySelector(".chosenPhoto");
+const choosePhotoContainer = document.querySelector(".choose-photo-container");
+const main = document.querySelector("main");
+const nav = document.querySelector("nav");
+
+main.style.height = `calc(100vh - ${nav.clientHeight}px - 3em)`;
+
+const reset = () => {
+  chosenPhoto.style.display = "none";
+  choosePhotoFileInput.value = null;
+  choosePhotoContainer.style.display = "block";
+};
 
 const choosePhotoBtnHandler = () => {
-    choosePhotoFileInput.click();
+  choosePhotoFileInput.click();
 };
 
 const fileInputOnchangeHandler = () => {
+  if (choosePhotoFileInput.files[0]) {
+    choosePhotoContainer.style.display = "none";
     const image = URL.createObjectURL(choosePhotoFileInput.files[0]);
     chosenPhoto.src = image;
     chosenPhoto.onload = () => {
-        URL.revokeObjectURL(image);
-    }
-}
+      URL.revokeObjectURL(image);
+    };
+    chosenPhoto.style.display = "block";
+  }
+};
+
+reset();
