629321e19c879ff31a1378084a44cae2fea6789d
Maha Magdy <mahamagdy3@gmail.com>
2023-04-06 23:13:26 +0200

    Add the functionality of the border of the photo

--- index.js
+++ index.js
@@ -1,45 +1,49 @@
-const photoContainer = document.querySelector('.photo-container');
+const photoContainer = document.querySelector(".photo-container");
 const choosePhotoBtn = document.querySelector(".choose-photo-btn");
 const choosePhotoFileInput = document.querySelector(".choose-photo-file-input");
 const chosenPhoto = document.querySelector(".chosenPhoto");
 const choosePhotoContainer = document.querySelector(".choose-photo-container");
 const main = document.querySelector("main");
 const nav = document.querySelector("nav");
-const controls = document.querySelectorAll('.controls');
+const controls = document.querySelectorAll(".controls");
 
 main.style.height = `calc(100vh - ${nav.clientHeight}px - 3em)`;
 
 const reset = () => {
-    chosenPhoto.style.display = "none";
-    choosePhotoFileInput.value = null;
-    choosePhotoContainer.style.display = "block";
-    document.documentElement.style.setProperty('--photo-bg-color', 'transparent');
+  chosenPhoto.style.display = "none";
+  choosePhotoFileInput.value = null;
+  choosePhotoContainer.style.display = "block";
+  document.documentElement.style.setProperty("--photo-bg-color", "transparent");
 };
 
 const choosePhotoBtnHandler = () => {
-    choosePhotoFileInput.click();
+  choosePhotoFileInput.click();
 };
 
 const fileInputOnchangeHandler = () => {
-    if (choosePhotoFileInput.files[0]) {
-        document.documentElement.style.setProperty('--photo-bg-color', 'white');
-        choosePhotoContainer.style.display = "none";
-        const image = URL.createObjectURL(choosePhotoFileInput.files[0]);
-        chosenPhoto.src = image;
-        chosenPhoto.onload = () => {
-            URL.revokeObjectURL(image);
-        };
-        chosenPhoto.style.display = "block";
-    }
+  if (choosePhotoFileInput.files[0]) {
+    document.documentElement.style.setProperty("--photo-bg-color", "white");
+    choosePhotoContainer.style.display = "none";
+    const image = URL.createObjectURL(choosePhotoFileInput.files[0]);
+    chosenPhoto.src = image;
+    chosenPhoto.onload = () => {
+      URL.revokeObjectURL(image);
+    };
+    chosenPhoto.style.display = "block";
+  }
 };
 
 function handleUpdate() {
-    const suffix = this.dataset.sizing || '';
-    document.documentElement.style.setProperty(`--${this.name}`, this.value);
-    console.log(this.value);
-    console.log(this.name);
+  const suffix = this.dataset.sizing || "";
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+  //   console.log(this.value + suffix);
+  //   console.log(this.name);
 }
 
-controls.forEach(input => input.addEventListener('change', handleUpdate));
+controls.forEach((input) => input.addEventListener("change", handleUpdate));
+// controls.forEach((input) => input.addEventListener("mousemove", handleUpdate));
 
 reset();
